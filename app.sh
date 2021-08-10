836be1aeb16de7123c57ff079e5876b919f5c030
pra <pra@mbarysas-t590>
2021-08-10 14:42:00 +0300

    frameworkification complete

--- app.sh
+++ app.sh
@@ -9,11 +9,16 @@ else
     exit 256
 fi
 
+if [[ -f /pki/certificate.pem && -f /pki/privatekey.pem ]]
+then
+    cat /pki/certificate.pem /pki/privatekey.pem > /tmp/everything.pem
+fi
+
 bootstrap_load_module $INPUT_INTERFACE
 bootstrap_load_module $OUTPUT_INTERFACE
 bootstrap_load_module util/ytdl
 bootstrap_load_module player/player
-
+bootstrap_load_module stream/stream
 
 util_ytdl::start_autoupdate
 
